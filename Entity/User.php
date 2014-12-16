522c32219d4b5c01563d0b7193a8b2c45b7e1756
Rudy Adamse <r.adamse@kr-solutions.nl>
2014-12-17 00:08:32 +0100

    added user entity

---
+++ Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KRSolutions\Bundle\KRUserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+}
