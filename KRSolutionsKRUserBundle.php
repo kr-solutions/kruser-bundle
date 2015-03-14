dc132d6bd87a6571d53334c8ca55ed49aaa24672
Rudy Adamse <r.adamse@kr-solutions.nl>
2015-03-14 20:34:29 +0100

    login form

--- KRSolutionsKRUserBundle.php
+++ KRSolutionsKRUserBundle.php
@@ -2,12 +2,34 @@
 
 namespace KRSolutions\Bundle\KRUserBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+
+/**
+ * K&R Solutions KRUserBundle
+ */
 class KRSolutionsKRUserBundle extends Bundle
 {
-    public function getParent()
-    {
-        return 'FOSUserBundle';
-    }
+
+	/**
+	 * Build
+	 *
+	 * @param ContainerBuilder $container
+	 */
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+	}
+
+	/**
+	 * Get the parent bundle
+	 *
+	 * @return string
+	 */
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
+
 }
